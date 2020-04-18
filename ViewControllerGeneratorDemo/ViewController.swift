67617d045e3e26c75c623f5ca4c540b2982003ea
Vic <iverson905117@gmail.com>
2020-04-18 22:15:49 +0800

    Initial Commit

---
+++ ViewControllerGeneratorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerGeneratorDemo
+//
+//  Created by 康志斌 on 2020/4/18.
+//  Copyright © 2020 AppChihPin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
